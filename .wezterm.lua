e5b3db3e13dbfeb56d9c54867ef556385290171d
Powerm1nt <leonard.chabot@nuka.works>
2025-04-16 01:52:50 +0200

    Rollback window deco's

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,5 +4,5 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Galaxy'
 config.font = wezterm.font '0xProto Nerd Font Mono'
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = 'NONE'
+-- config.window_decorations = 'NONE'
 return config
